05ffb7f955fa4f3cff63530de2c46673ac6ba591
hubchev <boitzen@yahoo.de>
2022-12-15 12:15:04 +0100

    set it up

---
+++ 22-10_dsb-project-description.R
@@ -0,0 +1,8 @@
+## ----setup, include=FALSE---------------------------------------
+library("knitr")
+knit_hooks$set(purl = hook_purl)
+
+## ----eval=FALSE-------------------------------------------------
+#  setwd("/home/sthu/Dropbox/hsf/github/courses/rmd/")
+#  rmarkdown::render("22-10_dsb-project-description.Rmd", "all")
+
---
+++ render_all.R
@@ -0,0 +1,4 @@
+install.packages("rmarkdown")
+library("rmarkdown")
+setwd("~/Dropbox/hsf/github/dsb-2023/")
+rmarkdown::render("22-10_dsb-project-description.Rmd", "all")
